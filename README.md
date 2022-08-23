725ff48590b0bfe7032f30bb712d8e7687f8fbf8
David Gaspar-Herrera <david.d.gaspar.h@gmail.com>
2022-08-23 12:54:12 -0500

    Added functionality for choosing color you would like to draw with, also fixed it so that last canvas dissaperas when user chooses to make new canvas

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # etch-a-sketch
-Browser version of something between a sketchpad and an Etch-A-Sketch.
+Changing name to pixl sketch
+gonna make it a site where srtist can make 
+pixel art and save it like a google docs 
+thing for pixel artI also want to make it so you can animate these things
--- script.js
+++ script.js
@@ -2,26 +2,33 @@
 //for each first iteration we make one row div and append 16 child divs into that through the other loop
 // then we put flex display flex direction column in order for the css to stack the row divs on top of eachother
 //then we have our grid I THINK
-let color;
-const red = document.querySelector('.red');
-const blue = document.querySelector('.blue');
-const green = document.querySelector('.green');
-red.addEventListener('click', () =>{
-    color = 'red'
-});
-green.addEventListener('click', () =>{
-    color = 'green'
-});
-blue.addEventListener('click', () =>{
-    color = 'blue'
-});
+
 function runIt(){
-    let userChoice;
+    let color = 'red';
+    const red = document.querySelector('.red');
+    const blue = document.querySelector('.blue');
+    const green = document.querySelector('.green');
+    red.addEventListener('click', () =>{
+        color = 'red';
+    });
+    green.addEventListener('click', () =>{
+        color = 'green';
+    });
+    blue.addEventListener('click', () =>{
+        color = 'blue';
+    });
+
     do {
         userChoice = prompt("Please choose the dimension of your square ie: entering 16 would make a 16 by 16 square");
     } while (userChoice > 100);
     let dimension = 500/(userChoice);
     let container = document.querySelector(".container");
+    if(container.childNodes.length != 0){
+        let grid = document.querySelectorAll('.grid');
+        grid.forEach(ting => {
+            ting.remove();
+        })
+    }
     for(let i = 0; i < userChoice; i++){
         let row = document.createElement('div');
         row.classList.add('row');
@@ -35,12 +42,12 @@ function runIt(){
     }
     const boxes = Array.from(document.querySelectorAll('.grid'));
     //boxes.forEach(box => box.setAttribute('style', `width: ${dimension}; height: white;`));
-    boxes.forEach(box => box.addEventListener('click', () => {
-        box.classList.add('color')
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = `${color}`
     }));
     boxes.forEach(box => box.addEventListener('contextmenu', function(ev) {
         ev.preventDefault();
-        box.classList.remove('color')
+        box.style.backgroundColor = 'white';
         return false;
     }));
 }
